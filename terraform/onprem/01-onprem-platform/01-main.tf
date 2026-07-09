2e7607d33668b7db004462ac6605542c13e2f1f7
hacksungboo <qngkrtjd@naver.com>
2026-07-09 16:42:21 +0900

    infra(argocd): onprem argocd 배포 helm 차트 버전 수정

--- terraform/onprem/01-onprem-platform/01-main.tf
+++ terraform/onprem/01-onprem-platform/01-main.tf
@@ -76,7 +76,7 @@ resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  version          = "7.3.7"
+  version          = "10.1.2"
   namespace        = "argocd"
   create_namespace = true
   values = [
